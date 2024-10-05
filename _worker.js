5fbcc68e2ae940b4b7c9cca5f0eb005201ad8b78
timiiify <64126096+timiiify@users.noreply.github.com>
2024-10-05 21:13:13 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -20,15 +20,11 @@ hysteria2://377cef77-6e39-4280-b5c9-429f1a60c40f@wap.kkbi.me:35727?&alpn=h3&inse
 tuic://377cef77-6e39-4280-b5c9-429f1a60c40f:377cef77-6e39-4280-b5c9-429f1a60c40f@wap.kkbi.me:63062?congestion_control=bbr&udp_relay_mode=native&alpn=h3&sni=wap.kkbi.me&allow_insecure=0#tu5-ubuntu
 https://o.clmurl2.com/api/v1/client/subscribe?token=12a05fb4d30d3793b7582d6d827902ed
 https://free.andiliba.cc/link/f2fa04a7dc6856dc?clash=1
-https://free.ninecloud.co/api/v1/client/subscribe?token=0b607fa078ff240fcd74449ef14525a7
-https://d7b12d59-21aa-9561-087f-89c834ac7fe8.372372.xyz/api/v1/client/subscribe?token=a0bd61df1dc976e3a3ddc37081f353a4
-https://dkcc71fg830ah.cloudfront.net/api/v1/client/subscribe?token=09c7ee8a7770e280ae364758040c0726
 https://xquigzxz2.youxuan.wiki/api/v1/client/subscribe?token=dd5ecfb47379d2b37599feb4b43931bf
 https://sub.xunlian.site//api/v1/client/subscribe?token=17b619d61ef041a48432223e4a70db2a
 https://d7b12d59-21aa-9561-087f-89c834ac7fe8.372372.xyz/api/v1/client/subscribe?token=a0dcd411f821385d1a7567d12fd8e66b
 https://davy520.pages.dev/96821107-ac42-438a-b781-a5999197a21c#%E8%87%AA%E5%BB%BA
 https://sub.mxlweb.xyz/free
-https://s1.trojanflare.one/subscription/shadowrocket/399d158d-bf14-41e0-8950-44ccebc11fbf
 https://pqjc.site/api/v1/client/subscribe?token=721c839e5146f6594ab607f23ee71acb
 https://v2.chatpro.icu/api/v1/client/subscribe?token=af539924756432c3abef2f7a8fa33eba
 https://sublink.52cloud.eu.org/api/v1/client/subscribe?token=8025d164-d726-40ed-af06-d7ea1138d1a4
